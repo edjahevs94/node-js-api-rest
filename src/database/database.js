b4b58379daa39e1538748a1ca4f1d49632cf3a6a
Edgard <edjahevs@gmail.com>
2022-05-28 20:45:24 -0500

    using heroku env varibles for db conections

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,8 @@
 import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize(
-    'ddqhccq2983ves', 
-    'bsnqiroctpjwnq', 
-    '7ad28655fd489969f6ab8b7d2bdee5b3d4c281db1b71b53ad03c9c60055c86b0', 
+  process.env.DATABASE_URL, 
     {
-        host: 'ec2-34-236-94-53.compute-1.amazonaws.com',
-        port: '5432',
-        dialect: 'postgres',
-        protocol: 'postgres',
         dialectOptions: {
           ssl: {
             require: true,
