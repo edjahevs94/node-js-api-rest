cd591ef8c8aa0a3fadba337173439838314b4eec
Edgard <edjahevs@gmail.com>
2022-06-02 14:09:39 -0500

    adding cors dependency

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
-import projectsRoutes from "./routes/projects.routes.js"
-import tasksRoutes from "./routes/tasks.routes.js"
+import projectsRoutes from "./routes/projects.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+import cors from "cors";
 
 const app = express();
 
 //añadir middlewares para poder entender los objetos json
 //que me puede enviar el cliente en formato body
+app.use(cors());
 app.use(express.json());
 
 app.use(projectsRoutes);
